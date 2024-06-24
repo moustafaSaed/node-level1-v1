3f4cae01a97f19b3674042e0b6196f24d162daca
moustafa <moustafasaed060@gmail.com>
2024-06-24 03:43:00 +0300

    step 2

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 4000
-
+const mongoose = require('mongoose');
 app.get('/', (req, res) => {
-    res.sendFile("./views/home.html", {root: __dirname});
+    res.sendFile("./views/home.html", { root: __dirname });
 })
 
-app.listen(port, () => {
-    console.log(`http://localhost:${port}/`);
+mongoose.connect('mongodb+srv://mostafasaed060:NKWmN9zuPy3X9d1y@mouscluster.w66om3n.mongodb.net/all-data?retryWrites=true&w=majority&appName=mousCluster').then(() => { // do if connect success
+    app.listen(port, () => {
+        console.log(`http://localhost:${port}/`);
+    })
+}).catch((err) => { // do if that's an error occur
+    console.log('ErrMous : ' + err);
 })
